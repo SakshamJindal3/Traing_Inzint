78102f2de64bd2ca09f9f3568421ca6a6aeb8c9d
Saksham Jindal <sakshamjindal3@gmail.com>
2023-01-25 17:45:24 +0530

    first commit

--- asyn.js
+++ asyn.js
@@ -1,15 +1,16 @@
-// setTimeout(myFunction, 3000);
+setTimeout(pFunction, 3000);
+
+function pFunction() {
+  console.log("Hlo 3 second passed");
+}
+// When you pass a function as an argument, remember not to use parenthesis.
+// Right: setTimeout(pFunction, 3000);
+// Wrong: setTimeout(pFunction(), 3000);
+
+// setInterval(myFunction, 1000);
 
 // function myFunction() {
-//   console.log("Hlo 3 second passed");
+//   let d = new Date();
+//   console.log(d.getHours() + ":" +d.getMinutes() + ":" +d.getSeconds());
 // }
-//When you pass a function as an argument, remember not to use parenthesis.
-//Right: setTimeout(myFunction, 3000);
-//Wrong: setTimeout(myFunction(), 3000);
 
-setInterval(myFunction, 1000);
-
-function myFunction() {
-  let d = new Date();
-  console.log(d.getHours() + ":" +d.getMinutes() + ":" +d.getSeconds());
-}
---
+++ clouser.js
@@ -0,0 +1,7 @@
+const add = (function () {
+  let counter = 0;
+  return function () {counter += 1; return counter}
+})();
+
+console.log(add());
+console.log(add());console.log(add());
